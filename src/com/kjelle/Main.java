a43d30b2c3b37cff11365284c689602bae340bb6
Kjelle <kallestralman@gmail.com>
2019-02-15 23:47:01 +0100

    + Added some error handling for arguments

--- src/com/kjelle/Main.java
+++ src/com/kjelle/Main.java
@@ -5,10 +5,15 @@ import com.kjelle.net.Server;
 public class Main {
 
     public static void main(String[] args) {
-        String[] socket = args[2].split(":");
-        String ip = socket[0];
-        int port = Integer.parseInt(socket[1]);
-        Server server1 = new Server(1337, port, ip);
-        server1.setMessage(args[0]);
+        if(args.length >= 0 && args.length < 2) {
+            String[] socket = args[2].split(":");
+            String ip = socket[0];
+            int port = Integer.parseInt(socket[1]);
+            Server server1 = new Server(1337, port, ip);
+            server1.setMessage(args[0]);
+        } else {
+            System.out.println("Wrong formatted arguments.\n" +
+                    "Must be 'java -jar sendUDP msg IP:port.\n");
+        }
     }
 }
