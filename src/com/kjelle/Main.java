0cf65ddcf3a1ad1f9273a2aee65bdcdc580e3f2d
Kjelle <kallestralman@gmail.com>
2019-02-16 01:07:36 +0100

    * Removed out of index error

--- src/com/kjelle/Main.java
+++ src/com/kjelle/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         if(args.length >= 0 && args.length < 2) {
-            String[] socket = args[2].split(":");
+            String[] socket = args[1].split(":");
             String ip = socket[0];
             int port = Integer.parseInt(socket[1]);
             Server server1 = new Server(1337, port, ip);
